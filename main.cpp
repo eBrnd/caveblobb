557f761d7180cace685192735f50ef658d722d81
eBrnd <github@ebrnd.de>
2011-05-30 14:20:54 +0200

    gah!

--- main.cpp
+++ main.cpp
@@ -10,17 +10,17 @@ int main()
   {
     fprintf(stderr, "Could not initialize SDL: %s\n", SDL_GetError());
     exit(1);
-
-    SDL_Surface *display;
-    display = SDL_SetVideoMode(800, 600, 16, SDL_SWSURFACE); // HWSURFACE?
-    if(display == NULL)
-    {
-      fprintf(stderr, "Could not initialize video: %s\n", SDL_GetError());
-      exit(1);
-    }
-
-    Game* game = new Game();
   }
+
+  SDL_Surface *display;
+  display = SDL_SetVideoMode(800, 600, 16, SDL_SWSURFACE); // HWSURFACE?
+  if(display == NULL)
+  {
+    fprintf(stderr, "Could not initialize video: %s\n", SDL_GetError());
+    exit(1);
+  }
+
+  Game* game = new Game(display);
 
   atexit(SDL_Quit);
 }
