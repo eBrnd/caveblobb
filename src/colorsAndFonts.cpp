7d8145068124a1b1843f9a3592552da431809e4c
eBrnd <github@ebrnd.de>
2011-06-27 00:35:45 +0200

    started using SFont

--- src/colorsAndFonts.cpp
+++ src/colorsAndFonts.cpp
@@ -4,11 +4,27 @@ ColorsAndFonts* ColorsAndFonts::pointer = NULL;
 
 ColorsAndFonts::ColorsAndFonts()
 {
+  font_surface = IMG_Load("/usr/local/share/caveblobb/font.png");
+  font2x_surface = IMG_Load("/usr/local/share/caveblobb/font2x.png");
+  font4x_surface = IMG_Load("/usr/local/share/caveblobb/font4x.png");
+  font8x_surface = IMG_Load("/usr/local/share/caveblobb/font8x.png");
+  
+  fonts[0] = SFont_InitFont(font_surface);
+  fonts[1] = SFont_InitFont(font2x_surface);
+  fonts[2] = SFont_InitFont(font4x_surface);
+  fonts[3] = SFont_InitFont(font8x_surface);
+
+  for(int i = 0; i < 4; i++)
+  {
+    if(fonts[i] == NULL)
+      std::cout << "Error loading font - text may not be renderes correctly." << std::endl;
+  }
+
   TTF_Init();
-  sans18 = TTF_OpenFont("/usr/share/fonts/TTF/FreeSans.ttf", 18);
-  sans43 = TTF_OpenFont("/usr/share/fonts/TTF/FreeSans.ttf", 43);
+  sans18 = TTF_OpenFont("/usr/local/share/caveblobb/FreeSans.ttf", 18);
+  sans43 = TTF_OpenFont("/usr/local/share/caveblobb/FreeSans.ttf", 43);
   if(sans18 == NULL || sans43 == NULL)
-    std::cout << "Error loading Font: /usr/share/fonts/TTF/FreeSans.ttf - Text may not be rendered properly." << std::endl;
+    std::cout << "Error loading Font: /usr/local/share/caveblobb/FreeSans.ttf - Text may not be rendered properly." << std::endl;
   white.r = white.b = white.g = 255;
   black.r = black.b = black.g = 0;
 }
@@ -20,4 +36,10 @@ ColorsAndFonts* ColorsAndFonts::getInstance()
     pointer = new ColorsAndFonts();
   }
   return pointer;
+}
+
+void ColorsAndFonts::write(SDL_Surface* display, int x, int y, int size, char* text)
+{
+  if(size < 4 && size >= 0 && fonts[size] != NULL)
+    SFont_Write(display, fonts[size], x, y, text);
 }
--- src/colorsAndFonts.hpp
+++ src/colorsAndFonts.hpp
@@ -5,17 +5,22 @@
 
 #include <SDL.h>
 #include <SDL_ttf.h>
+#include <SDL_image.h>
+#include "SFont.h"
 
 class ColorsAndFonts
 {
   private:
     ColorsAndFonts();
     static ColorsAndFonts* pointer;
+    SDL_Surface* font_surface, *font2x_surface, *font4x_surface, *font8x_surface;
+    SFont_Font* fonts[4];
   public:
     TTF_Font* sans18;
     TTF_Font* sans43;
     SDL_Color white, black;
     static ColorsAndFonts* getInstance();
+    void write(SDL_Surface* display, int x, int y, int size, char* text);
 };
 
 #endif
